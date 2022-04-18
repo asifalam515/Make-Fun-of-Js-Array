3b8100dd49d2c7946bb222292a8ab79cdb1af5d6
asifalam515 <asibulalam515@gmail.com>
2022-04-18 19:54:18 +0600

    second commit

---
+++ hellow.js
@@ -0,0 +1,8 @@
+// another two way we can declare array
+// lets see the example
+// 1.constractor pattern;
+const arr1=new Array(21,3456,46,677)
+// console.log(arr1);
+// Factory pattern
+const arr2=Array(324,565,76,7652,34)
+console.log(arr2,arr2.length);
